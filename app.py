5c7d867bb8095d8bef0261e54b401c79c1fe5415
Timtonix <timtonic@outlook.fr>
2024-02-23 16:12:05 +0100

    page HTML d'inscription

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = "banane"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/inscription")
+def inscription():
+    return render_template("inscription.html")
+
+if __name__ == "__main__":
+    app.run(port=5555)
