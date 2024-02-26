d5e2ac528b4753637002b2f475a84da10866c09f
timtonix <timtonic@outlook.fr>
2024-02-26 10:22:42 +0100

    génération du code très sécurisé

--- utils/verification.py
+++ utils/verification.py
@@ -1,4 +1,12 @@
 import bdd
+import random
+import string
 
-def create_verification_code():
-    devlobdd = bdd.DevloBDD()
+
+def create_verification_code() -> str:
+    length = 4
+    code = ""
+    for i in range(length):
+        code += random.choice(string.ascii_letters)
+        code += str(random.randint(0, 9))
+    return code
