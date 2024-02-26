abfea01adf826fca9231703bf0126636dfd223db
timtonix <timtonic@outlook.fr>
2024-02-26 09:58:09 +0100

    préparation email et vérif

---
+++ utils/email_api.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+
+
+
+def send_verification_email(target, verification_code):
+    pass
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "devlowave.offi@gmail.com"  # Enter your address
+receiver_email = "timtonic2@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
