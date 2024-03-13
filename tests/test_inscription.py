550598da743e019cb642fe4663f07cd170fdc138
Timtonix <timtonic@outlook.fr>
2024-03-13 15:57:19 +0100

    tentative des tests unitaires

---
+++ tests/test_inscription.py
@@ -0,0 +1,11 @@
+from app import app
+import pytest
+
+def test_mauvais_id():
+    response = app.test_client().post('/inscription', data={
+        "email": "timtonix@icloud.com",
+        "ja_id": "azerty?",
+        "password": "aefkdnbùgkfxdgbmfvlkbeshf"
+    })
+    assert response.status_code == 200
+
