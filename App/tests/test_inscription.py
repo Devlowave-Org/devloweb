b10085e708a67aa457ae2a268ac8b6e5595c310a
timtonix <timtonic@outlook.fr>
2024-03-14 09:37:47 +0100

    email et password

--- App/tests/test_inscription.py
+++ App/tests/test_inscription.py
@@ -6,11 +6,30 @@ def test_mauvais_id():
         "ja_id": "azerty?",
         "password": "aefkdnbùgkfxdgbmfvlkbeshf"
     })
-    print(response.status_code)
 
     assert response.status_code == 200
     assert b"Invalid JA ID" in response.data
 
+def test_mauvais_mail():
+    response = app.test_client().post('/inscription', data={
+        "email": "timtoniicloud.com",
+        "ja_id": "JA-8166",
+        "password": "eqfliuherlughdslfher"
+    })
+
+    assert response.status_code == 200
+    assert b"Veuillez remplir un email valide" in response.data
+
+
+def test_mauvais_password():
+    response = app.test_client().post('/inscription', data={
+        "email": "timtonix@icloud.com",
+        "ja_id": "JA-8166",
+        "password": "azerty..."  # -12 caractères
+    })
+
+    assert response.status_code == 200
+    assert 'Veuillez avoir un mot de passe d&#39;au moins 12 caractères'.encode("utf-8") in response.data
 
 def test_simple():
     response = app.test_client().get("/")
