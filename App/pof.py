6f1fffa4a0553219c47335685d11551c1a93fa34
Timtonix <timtonic@outlook.fr>
2024-06-01 12:49:33 +0200

    Formulaire HTML

--- App/pof.py
+++ App/pof.py
@@ -1,5 +1,8 @@
-from flask import render_template, Flask, session, redirect, url_for, g, has_app_context
+from flask import render_template, Flask, session, redirect, url_for, g, has_app_context, request
 
 
-def proof_of_concept():
+def proof_of_concept(db):
+    if request.method == "POST":
+
+
     return render_template("pof.html")
