77926bf940c6ab1cf51d6a8786d1e36ec6d57464
Timtonix <timtonic@outlook.fr>
2024-06-04 09:05:36 +0200

    j'espère que ça utilise bien le meme client

--- App/tests/test_editeur.py
+++ App/tests/test_editeur.py
@@ -1,15 +1,19 @@
 import os
 import pytest
 from test_inscription import devlobdd, req_connection, req_code_verif
-from devloapp import app, session
+from devloapp import app
 
 app.which = "devlotest"
 os.system("rm -rf ~/PycharmProjects/devloweb/devlotest.db")
+os.system("rm -rf devlotest.db")
 
 
-def setup_account(devlobdd):
+
+
+
+def setup_account(client, devlobdd):
     devlobdd.delete_ja("timtonix@icloud.com")
-    app.test_client().post('/inscription', data={
+    client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "JA-8166",
         "password": "jesuisunebananeavecdespouvoirsmagiques"  # +12 caractères
@@ -19,12 +23,13 @@ def setup_account(devlobdd):
     req_connection("timtonix@icloud.com", "jesuisunebananeavecdespouvoirsmagiques")
 
 
-def test_editor_access_without_account():
-    response = app.test_client().get('/home/editeur')
+def test_editor_access_without_account(client):
+    response = client.get('/home/editeur')
     assert response.status_code == 302
 
-def test_good_editor_access(devlobdd):
-    setup_account(devlobdd)
+
+def test_good_editor_access(client, devlobdd):
+    setup_account(client, devlobdd)
     response = app.test_client().get('/home/editeur')
 
     assert response.status_code == 200
--- App/tests/test_inscription.py
+++ App/tests/test_inscription.py
@@ -3,17 +3,16 @@ import time
 from datetime import datetime
 from App.utils.bdd import DevloBDD
 import pytest
-from devloapp import app
 
 
-app.which = "devlotest"
-os.system("rm -rf ~/PycharmProjects/devloweb/devlotest.db")
+
 @pytest.fixture()
 def devlobdd():
     return DevloBDD("devlotest")
 
-def test_mauvais_id(devlobdd):
-    response = app.test_client().post('/inscription', data={
+
+def test_mauvais_id(client, devlobdd):
+    response = client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "azerty?",
         "password": "aefkdnbùgkfxdgbmfvlkbeshf"
@@ -24,8 +23,8 @@ def test_mauvais_id(devlobdd):
 
 
 
-def test_mauvais_mail(devlobdd):
-    response = app.test_client().post('/inscription', data={
+def test_mauvais_mail(client, devlobdd):
+    response = client.post('/inscription', data={
         "email": "timtoniicloud.com",
         "ja_id": "JA-8166",
         "password": "eqfliuherlughdslfher"
@@ -37,8 +36,8 @@ def test_mauvais_mail(devlobdd):
 
 
 
-def test_mauvais_password(devlobdd):
-    response = app.test_client().post('/inscription', data={
+def test_mauvais_password(client, devlobdd):
+    response = client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "JA-8166",
         "password": "azerty..."  # -12 caractères
@@ -50,8 +49,8 @@ def test_mauvais_password(devlobdd):
 
 
 
-def test_champ_manquant(devlobdd):
-    response = app.test_client().post('/inscription', data={
+def test_champ_manquant(client, devlobdd):
+    response = client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "JA-8166",
         "password": ""  # -12 caractères
@@ -63,9 +62,9 @@ def test_champ_manquant(devlobdd):
 
 
 
-def test_good_inscription(devlobdd):
+def test_good_inscription(client,devlobdd):
     devlobdd.delete_ja("timtonix@icloud.com")
-    response = app.test_client().post('/inscription', data={
+    response = client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "JA-8166",
         "password": "jesuisunebananeavecdespouvoirsmagiques"  # +12 caractères
@@ -76,8 +75,8 @@ def test_good_inscription(devlobdd):
     assert devlobdd.get_ja_by_mail("timtonix@icloud.com")[4] == 0
 
 
-def test_account_already_exists():
-    response = app.test_client().post('/inscription', data={
+def test_account_already_exists(client):
+    response = client.post('/inscription', data={
         "email": "timtonix@icloud.com",
         "ja_id": "JA-8166",
         "password": "jesuisuneAUTREbananeavecdespouvoirsmagiques"  # -12 caractères
@@ -86,56 +85,56 @@ def test_account_already_exists():
     assert 'Vous avez déjà un compte' in response.data.decode('utf-8')
 
 
-def req_code_verif(ja_id, code):
-    resp = app.test_client().post('/verification', data={
+def req_code_verif(client, ja_id, code):
+    resp = client.post('/verification', data={
         "ja_id": ja_id,
         "verif": code
     })
     return resp
 
 
-def test_mauvais_code_verif(devlobdd):
-    response = req_code_verif("JA-8166", 1234)
+def test_mauvais_code_verif(client, devlobdd):
+    response = req_code_verif(client, "JA-8166", 1234)
 
     assert response.status_code == 200
     assert devlobdd.get_ja_by_mail("timtonix@icloud.com")[4] == 0
     assert devlobdd.get_try("127.0.0.1")[1] == 1
 
 
-def test_punition_verif_code(devlobdd):
+def test_punition_verif_code(client, devlobdd):
     devlobdd.delete_try("127.0.0.1")
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert response.status_code == 200
     assert devlobdd.get_try("127.0.0.1")[1] == 1
-    response = req_code_verif("JA-8166", 1234)
-    response = req_code_verif("JA-8166", 1234)
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client, "JA-8166", 1234)
+    response = req_code_verif(client, "JA-8166", 1234)
+    response = req_code_verif(client, "JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 4
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 5
     punish_date = datetime.strptime(devlobdd.get_try("127.0.0.1")[4], "%Y-%m-%d %H:%M:%S.%f")
     delta = punish_date - datetime.now()
     assert punish_date > datetime.now()
     assert 1700 < delta.seconds < 1900
-    response = req_code_verif("JA-8166", 1234)
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 5
 
 
-def test_code_verif_after_punished(devlobdd):
+def test_code_verif_after_punished(client, devlobdd):
     # Bon en gros on reset pas la BDD après la punition ci-dessus. Et donc meme si on à le bon code ça marche pas
     code = devlobdd.get_code_via_jaid("8166")[1]
-    response = req_code_verif("JA-8166", code)
+    response = req_code_verif(client,"JA-8166", code)
     assert devlobdd.get_try("127.0.0.1")[1] == 5
     assert devlobdd.get_ja_by_mail("timtonix@icloud.com")[4] == 0
     assert response.status_code == 200
 
 
 
-def test_good_code_verification(devlobdd):
+def test_good_code_verification(client, devlobdd):
     devlobdd.delete_try("127.0.0.1")
     code = devlobdd.get_code_via_jaid("8166")[1]
-    response = req_code_verif("JA-8166", code)
+    response = req_code_verif(client,"JA-8166", code)
     assert response.status_code == 302
     # Le compte existe et il est activé
     assert devlobdd.get_ja_by_mail("timtonix@icloud.com")[4] == 1
@@ -152,18 +151,18 @@ def test_already_activated_ja(devlobdd):
 
 
 @pytest.mark.slow
-def test_wait_punition_time(devlobdd):
+def test_wait_punition_time(client,devlobdd):
     # On l'inscrit
-    test_good_inscription(devlobdd)
+    test_good_inscription(client, devlobdd)
     # On attend 30 minutes après la punition et on peut ensuite activer le compte
-    test_punition_verif_code(devlobdd)
+    test_punition_verif_code(client,devlobdd)
     # On désactive la JA manuellement au cas ou elle  était déjà ativé à cause du test du dessus
     devlobdd.desactiver_ja("JA-8166")
     time.sleep(1810)
     code = devlobdd.get_code_via_jaid("8166")[1]
     response = req_code_verif("JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 1
-    response = req_code_verif("JA-8166", code)
+    response = req_code_verif(client,"JA-8166", code)
     # Il ne s'active pas car le code a plus de 30 minutes
     assert response.status_code == 200
     # Le compte existe et il n'est pas activé
@@ -171,73 +170,73 @@ def test_wait_punition_time(devlobdd):
 
 
 @pytest.mark.slow
-def test_reset_try(devlobdd):
+def test_reset_try(client,devlobdd):
     devlobdd.delete_try("127.0.0.1")
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert response.status_code == 200
     assert devlobdd.get_try("127.0.0.1")[1] == 1
-    response = req_code_verif("JA-8166", 1234)
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 3
     time.sleep(610)
-    response = req_code_verif("JA-8166", 1234)
+    response = req_code_verif(client,"JA-8166", 1234)
     assert devlobdd.get_try("127.0.0.1")[1] == 1
 
 
-def req_connection(mail, password):
-    resp = app.test_client().post('/connexion', data={
+def req_connection(client, mail, password):
+    resp = client.post('/connexion', data={
         "email": mail,
         "password": password
     })
     return resp
 
 
-def test_bad_mail_connexion(devlobdd):
+def test_bad_mail_connexion(client,devlobdd):
     devlobdd.delete_try("127.0.0.1")
-    resp = req_connection("jambon@.com", "azertyuiopqsdfghjklm")
+    resp = req_connection(client,"jambon@.com", "azertyuiopqsdfghjklm")
     assert resp.status_code == 200
     assert devlobdd.get_try("127.0.0.1") is None
     assert "Veuillez rentrer un vrai email" in resp.data.decode("utf-8")
 
     # Ici le mail est bon dans sa forme mais n'a pas de compte
-    resp = req_connection("timtonix@gmail.com", "azertyuiopqsdfghjklm")
+    resp = req_connection(client,"timtonix@gmail.com", "azertyuiopqsdfghjklm")
     assert resp.status_code == 200
     assert devlobdd.get_try("127.0.0.1")[1] == 1
     assert "Mail ou mot de passe incorrect" in resp.data.decode("utf-8")
 
 
 
-def test_wrong_password_connection(devlobdd):
+def test_wrong_password_connection(client,devlobdd):
     devlobdd.delete_try("127.0.0.1")
-    resp = req_connection("timtonix@icloud.com", "azertyuiopqsdfghjklm")
+    resp = req_connection(client,"timtonix@icloud.com", "azertyuiopqsdfghjklm")
     assert resp.status_code == 200
     assert "Mail ou mot de passe incorrect" in resp.data.decode("utf-8")
     assert devlobdd.get_try("127.0.0.1")[1] == 1
 
 
-def test_good_connection(devlobdd):
+def test_good_connection(client, devlobdd):
     devlobdd.delete_try("127.0.0.1")
-    resp = req_connection("timtonix@icloud.com", "jesuisunebananeavecdespouvoirsmagiques")
+    resp = req_connection(client, "timtonix@icloud.com", "jesuisunebananeavecdespouvoirsmagiques")
     assert resp.status_code == 302
     assert devlobdd.get_try("127.0.0.1") is None
 
 
 @pytest.mark.slow
-def test_ask_new_code(devlobdd):
+def test_ask_new_code(client, devlobdd):
     devlobdd.reset_bdd()
     devlobdd.__init__("devlotest")
     # On simule une inscritpion
     test_good_inscription(devlobdd)
     assert devlobdd.get_code_via_jaid("JA-8166") is not None
     code = devlobdd.get_code_via_jaid("JA-8166")[1]
-    resp = app.test_client().post('/resend', data={
+    resp = client.post('/resend', data={
         "ja_id": "JA-8166"
     })
     assert resp.status_code == 200
     # On vérifie que le code n'a pas changé
     assert devlobdd.get_code_via_jaid("JA-8166")[1] == code
     time.sleep(120)
-    resp = app.test_client().post('/resend', data={
+    resp = client.post('/resend', data={
         "ja_id": "JA-8166"
     })
     # Le code a bien été mis à jour
@@ -247,13 +246,13 @@ def test_ask_new_code(devlobdd):
     test_good_code_verification()
 
 
-def test_ask_new_code_already_active(devlobdd):
+def test_ask_new_code_already_active(client, devlobdd):
     devlobdd.reset_bdd()
     devlobdd.__init__("devlotest")
     # On simule une inscritpion
-    test_good_inscription(devlobdd)
-    test_good_code_verification(devlobdd)
-    resp = app.test_client().post('/resend', data={
+    test_good_inscription(client, devlobdd)
+    test_good_code_verification(client, devlobdd)
+    resp = client.post('/resend', data={
         "ja_id": "JA-8166"
     })
     # On ne lui a pas crée de nouveau code
