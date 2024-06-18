7e1db1380bbc4387376078435545151764e11a08
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-18 11:37:46 +0200

    📝 Session Fix and templates

--- App/home.py
+++ App/home.py
@@ -10,25 +10,25 @@ def parametres_generaux(devlobdd):
     if request.method == 'POST':
         form_data = request.form.to_dict(flat=False)  # Convert ImmutableMultiDict to regular dict
         print(form_data)
-        devlobdd.boom_boom(form_data, session['ja'])
-    return render_template('home/parametres_generaux.html', data=devlobdd.get_site_by_ja(session['ja']))
+        devlobdd.boom_boom(form_data, session['ja_id'])
+    return render_template('home/parametres_generaux.html', data=devlobdd.get_site_by_ja(session['ja_id']))
 
 def parametres_theme(devlobdd):
     if request.method == 'POST':
-        devlobdd.change_theme(session['ja'], request.form.get('theme'))
-    return render_template('home/parametres_theme.html', data=devlobdd.get_site_by_ja(session['ja']))
+        devlobdd.change_theme(session['ja_id'], request.form.get('theme'))
+    return render_template('home/parametres_theme.html', data=devlobdd.get_site_by_ja(session['ja_id']))
                                                                                          
 def site_verification(devlobdd):
-    return render_template('home/verification.html', data=devlobdd.get_site_by_ja(session['ja']))
+    return render_template('home/verification.html', data=devlobdd.get_site_by_ja(session['ja_id']))
 
 def domaine(devlobdd):
     if request.method == 'POST':
         name = request.form.get('name')
         domain = request.form.get('domain')
-        data=devlobdd.get_site_by_ja(session['ja'])
+        data=devlobdd.get_site_by_ja(session['ja_id'])
         if data[1] == f"{name.lower()}.{domain.lower()}":
             flash("Rien n'as changé !")
-            data=devlobdd.get_site_by_ja(session['ja'])
+            data=devlobdd.get_site_by_ja(session['ja_id'])
             return render_template('home/domaine.html', data=data)
         
         if cloudflare.subdomain_exist(name, domain) == False:
@@ -36,15 +36,15 @@ def domaine(devlobdd):
             test = cloudflare.create_subdomain(name, "1.2.3.4", domain)
             if test == True:
                 cloudflare.delete_subdomain({data[1]}, {data[2]})
-                devlobdd.change_domain(session['ja'], name.lower(), domain.lower())
+                devlobdd.change_domain(session['ja_id'], name.lower(), domain.lower())
                 flash("Votre domaine a été enregistré ! ")
-                data=devlobdd.get_site_by_ja(session['ja'])
+                data=devlobdd.get_site_by_ja(session['ja_id'])
                 return render_template('home/domaine.html', data=data)
             else:
                 flash("Erreur : " + str(test))
         else:
             flash("Erreur : ce nom de domaine existe de déjà !")
-    data=devlobdd.get_site_by_ja(session['ja'])
+    data=devlobdd.get_site_by_ja(session['ja_id'])
     return render_template('home/domaine.html', data=data)
 
 def add_page():
