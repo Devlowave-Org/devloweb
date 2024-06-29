8ccc1cb9b8c37073e7561046f94f769f5fd81904
Timtonix <timtonic@outlook.fr>
2024-06-29 15:37:58 +0200

    Refactor propre

--- App/home.py
+++ App/home.py
@@ -20,22 +20,6 @@ def editeur():
 
     return render_template("editor/v1/editeur.html", data=json_site)
 
-def v1():
-    if 'email' not in session:
-        return redirect(url_for('route_connexion'))
-
-    json_site = json.loads(open(f"tmp/{session['ja_id']}/site.json").read())
-    print(json_site)
-
-    if request.method == "POST":
-        form = request.form.to_dict()
-        with open(f"tmp/{session['ja_id']}/site.json", "w") as f:
-            json.dump(form, f)
-        print(form)
-
-    return render_template("editor/v1/editeur.html", data=json_site)
-
-
 
 def parametres_theme(devlobdd):
     if request.method == 'POST':
