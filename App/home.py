46172f19a670e79a1b95237769cae8ea1fa7e56f
Timtonix <timtonic@outlook.fr>
2024-08-27 16:09:26 +0200

    demande d'hébergement + status + ndd

--- App/home.py
+++ App/home.py
@@ -1,9 +1,11 @@
+import re
+
 from flask import request, render_template, session, redirect, flash, url_for
 from App.utils import utils, cloudflare
 import json
 
 
-def index():
+def index(devlobdd):
     return render_template('home/index.html')
 
 
@@ -19,9 +21,15 @@ def editeur():
     return render_template("editor/beta/editeur.html", data=json_site)
 
 def hebergement(devlobdd):
-    if request.method == "POST":
-        print("Reçu")
-    return render_template("home/hebergement.html")
+    status_dict = {0: "Désactivé", 1: "Hébergé", 2: "En attente", 3: "Refusé"}
+    site = devlobdd.get_site_by_ja(session['ja_id'])
+    if request.method == "POST" and request.form["heberger"] == "heberger":
+        if not re.fullmatch(r"[a-z0-9]{1,15}", request.form["domain"]):
+            return render_template("home/hebergement.html", error="Le nom de domaine doit contenir des lettres minuscules et ne doit avoir plus de 15 caractères")
+        devlobdd.ask_hebergement(session['ja_id'])
+        devlobdd.set_domain_name(session['ja_id'], request.form["domain"])
+
+    return render_template("home/hebergement.html", status=status_dict[site[3]], domain=site[1])
 
 
 def account():
