1f4de34ddfc79a14bc7a00eee156223e7f6fb095
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-08-28 12:23:02 +0200

    🎨 Switch à tailwind compilé

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "selector",
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: [
+          "Red Hat Display",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        sans: [
+          "Red Hat Display",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
