7a7a1e2c3ab1bd899ea47e80789de97ee3d19ce5
timtonix <timtonic@outlook.fr>
2024-09-04 09:50:28 +0200

    start again

--- test_env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-gmail_app = os.environ.get("GMAIL_APP")
-server_name = os.environ.get("SERVER_NAME")
-env = os.environ.get("ENV")
-
-print("GMAIL_APP:", gmail_app)
-print("SERVER_NAME:", server_name)
-print("ENV:", env)
