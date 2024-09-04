b4ef9c680685989f801b6f562891603d173c9da5
timtonix <timtonic@outlook.fr>
2024-09-04 12:52:00 +0200

    fix get_db

--- devloapp.py
+++ devloapp.py
@@ -111,7 +111,7 @@ def route_editeur():
 @app.route("/home/preview", methods=("GET", "POST"))
 def route_preview():
     # C'est le DASHBOARD Éditeur
-    if is_connected(session, get_db()):
+    if is_connected(session, db):
         return home.preview(session["ja_id"])
     return redirect(url_for('route_connexion'))
 
