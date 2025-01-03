1a9fd185b50dbd3ad0217e3d172419a951efc1bc
Timtonix <timtonic@outlook.fr>
2025-01-03 15:59:23 +0100

    clé secrete

--- devloapp.py
+++ devloapp.py
@@ -22,6 +22,9 @@ if environ.keys().__contains__("SERVER_NAME") and environ["ENV"] == "prod":
     app.config["SERVER_NAME"] = environ["SERVER_NAME"]
     db = DevloBDD(environ["DB_USERNAME"], environ["DB_PASSWORD"], "localhost", 3306)
 
+if environ["ENV"] == "dev":
+    app.config["DEBUG"] = True
+
 elif environ["ENV"] == "test":
     app.config["SERVER_NAME"] = "127.0.0.1:5555"
     db = DevloBDD(environ["DB_USERNAME"], environ["DB_PASSWORD"], "localhost", 3306, database="devlotest")
